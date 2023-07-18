6269dcc825cb3172b3b38dc3247dfcfc37cab1bf
Sammy Weller <sammymermell@gmail.com>
2023-07-18 11:51:50 -0400

    added routing to app

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,7 +5,7 @@ import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 
 
@@ -16,7 +16,6 @@ export const MainView = () => {
   const [user, setUser] = useState(storedUser? storedUser : null);
   const [token, setToken] = useState(storedToken? storedToken : null);
   const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
 
 
 
@@ -34,7 +33,7 @@ export const MainView = () => {
 
         const moviesFromApi = data.map((movie) => {
           return {
-            _id: movie.id,
+            id: movie._id,
             title: movie.title,
             imagePath: movie.imagePath,
             description: movie.description,
@@ -53,7 +52,7 @@ export const MainView = () => {
 
   //https://sw-myflix-app-baa5e3f40824.herokuapp.com/movies
 
-
+  /*
   return (
     <Row className="justify-content-md-center">
       {!user ? (
@@ -107,4 +106,83 @@ export const MainView = () => {
       )}
     </Row>
   );
+};
+
+*/
+
+
+return (
+  <BrowserRouter>
+    <Row className="justify-content-md-center">
+      <Routes>
+        <Route
+          path="/signup"
+          element={
+            <>
+              {user ? (
+                <Navigate to="/" />
+              ) : (
+                <Col md={5}>
+                  <SignupView />
+                </Col>
+              )}
+            </>
+
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <>
+              {user ? (
+                <Navigate to="/" />
+              ) : (
+                <Col md={5}>
+                  <LoginView onLoggedIn={(user) => setUser(user)} />
+                </Col>
+              )}
+            </>
+
+          }
+        />
+        <Route
+          path="/movies/:movieId"
+          element={
+            <>
+              {!user ? (
+                <Navigate to="/login" replace />
+              ) : movies.length === 0 ? (
+                <Col>The list is empty!</Col>
+              ) : (
+                <Col md={8}>
+                  <MovieView movies={movies} />
+                </Col>
+              )}
+            </>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <>
+              {!user ? (
+                <Navigate to="/login" replace />
+              ) : movies.length === 0 ? (
+                <Col>The list is empty!</Col>
+              ) : (
+                <>
+                  {movies.map((movie) => (
+                    <Col className="mb-4" key={movie.id} md={3}>
+                      <MovieCard movie={movie} />
+                    </Col>
+                  ))}
+                </>
+              )}
+            </>
+          }
+        />
+      </Routes>
+    </Row>
+  </BrowserRouter>
+);
 };
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,20 +1,23 @@
 // Import the PropTypes library
+import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 import "./movie-card.scss";
 
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
 
   return (
     <Card className="h-100" style={{ border: "0px solid rgba(0, 0, 0, 0)", backgroundColor: "rgba(0, 0, 0, 0.3)"}}>
       <Card.Img variant="top" src={movie.imagePath} />
       <Card.Body >
-        <Card.Title class="movieTitle">{movie.title}</Card.Title>
-        <Card.Text class="director">Director: {movie.director.name}</Card.Text>
-        <Button onClick={() => onMovieClick(movie)}  >
-          Open
-        </Button>
+        <Card.Title className="movieTitle">{movie.title}</Card.Title>
+        <Card.Text className="director">Director: {movie.director.name}</Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+          <Button variant="link">Open</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
@@ -25,5 +28,5 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  //onMovieClick: PropTypes.func.isRequired
 };
